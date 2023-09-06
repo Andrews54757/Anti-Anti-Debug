d2f2e80e129b7118dd84f1b47945eebc7df2ac00
Andrew S <andrews54757@gmail.com>
2023-09-06 02:14:29 -0500

    v1.0.1

--- anti-anti-debug.js
+++ anti-anti-debug.js
@@ -1,8 +1,5 @@
 !(() => {
     console.log("Anti-anti-debug loaded! Happy debugging!")
-    var _open = (a, b, c) => {
-        console.log("tried open", a, b, c);
-    };
     var _createElement = document.createElement;
     document.createElement = function (el, o) {
         var string = el.toString();
@@ -19,46 +16,117 @@
         return element;
     }
 
-    var _clear = window.console.clear;
-
-    window.console.clear = function () {
-
+    let cutoffs = {
+        table: {
+            amount: 5,
+            within: 5000
+        },
+        clear: {
+            amount: 5,
+            within: 5000
+        },
+        redactedLog: {
+            amount: 5,
+            within: 5000
+        },
+        debugger: {
+            amount: 10,
+            within: 10000
+        },
+        interval: {
+            amount: 10,
+            within: 10000
+        }
     }
 
     var _log = window.console.log;
+ 
+    function shouldLog(type) {
+        const cutoff = cutoffs[type];
+        if (cutoff.tripped) return false;
+        cutoff.current = cutoff.current || 0;
+        const now = Date.now();
+        cutoff.last = cutoff.last || now;
+
+        if (now - cutoff.last > cutoff.within) {
+            cutoff.current = 0;
+        }
+
+        cutoff.last = now;
+        cutoff.current++;
+
+        if (cutoff.current > cutoff.amount) {
+            _log("Limit reached! Will now ignore " + type)
+            cutoff.tripped = true;
+            return false;
+        }
+
+        return true;
+    }
 
     window.console.log = function () {
         var args = Array.from(arguments);
-        _log.apply(console, args.map((a) => {
 
-            if (typeof a === 'function') return "Redacted Function";
+        let redactedCount = 0;
+        const newArgs = args.map((a) => {
+
+            if (typeof a === 'function') {
+                redactedCount++;
+                return "Redacted Function";
+            }
             if (typeof a !== 'object' || a === null) return a;
             var props = Object.getOwnPropertyDescriptors(a)
             for (var name in props) {
-                if (props[name].get !== undefined) return "Redacted";
+                if (props[name].get !== undefined) {
+                    redactedCount++;
+                    return "Redacted";
+                }
                 if (name === 'toString') {
+                    redactedCount++;
                     return "Redacted Str";
                 }
             }
+
+            // Defeat Performance Detector
+            // https://github.com/theajack/disable-devtool/blob/master/src/detector/sub-detector/performance.ts
+            if (Array.isArray(a) && a.length === 50 && typeof a[0] === "object") {
+                redactedCount++;
+                return "Redacted ObjArray";
+            }
+
             return a;
-        }))
+        });
+
+        if (redactedCount >= Math.max(args.length - 1, 1)) {
+            if (!shouldLog("redactedLog")) {
+                return;
+            }
+        }
+
+        _log.apply(console, newArgs)
     }
+   
     window.console.table = function (obj) {
-        console.log("Redacted table")
+        if (shouldLog("table")) {
+            _log("Redacted table");
+        }
     }
+
     window.console.clear = function () {
-        console.log("Prevented clear")
+        if (shouldLog("table")) {
+            _log("Prevented clear");
+        }
     }
 
-})();
-
-(() => {
     var _constructor = window.Function.prototype.constructor;
     // Hook Function.prototype.constructor
     window.Function.prototype.constructor = function () {
         var fnContent = arguments[0];
         if (fnContent) {
             if (fnContent.includes('debugger')) { // An anti-debugger is attempting to stop debugging
+                if (shouldLog("debugger")) {
+                    _log("Prevented debugger");
+                }
                 return (function () { });
             }
         }
@@ -66,5 +134,26 @@
         return _constructor.apply(this, arguments);
     };
     window.Function.prototype.constructor.prototype = _constructor.prototype;
+
+    const oldSetInterval = window.setInterval;
+    const nativeToString = Function.prototype.toString;
+    window.setInterval = function (fn, ...args) {
+        const string = nativeToString.apply(fn);
+
+        // Regexp tests for https://github.com/theajack/disable-devtool
+        if (/if.*;try.*=.*catch.*\(.*\).*finally.*==.*typeof/.test(string)) {
+            if (shouldLog("interval")) {
+                _log("Prevented anti-debug interval (matched regexp)");
+            }
+            return;
+        } else
+        if (string.includes('debugger')) {
+            if (shouldLog("debugger")) {
+                _log("Prevented debugger");
+            }
+            return;
+        }
+        return oldSetInterval(fn, ...args);
+    }
 })()
 
